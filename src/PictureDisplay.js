bd4d23336dab052b6c43db5d8772c19ea277ffad
Bradley Landis <bradleylandis@bradleylandis.com>
2018-01-13 10:55:47 -0500

    use string interpolation

--- src/PictureDisplay.js
+++ src/PictureDisplay.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const PictureDisplay = ({numberOfIncorrectGuesses}) => {
-    return <img alt={'hangman with ' + numberOfIncorrectGuesses + ' incorrect guesses'} src={'hangman' + numberOfIncorrectGuesses + '.png'}/>
+    return <img alt={`hangman with ${numberOfIncorrectGuesses} incorrect guesses`} src={`hangman${numberOfIncorrectGuesses}.png`}/>
 }
 
 export default PictureDisplay
