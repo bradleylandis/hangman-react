2ec6d811f0a7c7eb4e270e820362cd8b849ab3e4
Bradley Landis <bradleylandis@bradleylandis.com>
2018-01-24 13:32:07 -0500

    make the game easier

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
 const api = () => {
     function fetchWord() {
-        return fetch('http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=true&excludePartOfSpeech=proper-noun&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit=1&api_key=503890e5c73712c79d3090fb3840a8220541b1c15372a08d8')
+        return fetch('http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=true&includePartOfSpeech=noun,verb,adjective,adverb,preposition,conjunction,verb-transitive&minCorpusCount=1000000&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=7&maxLength=15&api_key=503890e5c73712c79d3090fb3840a8220541b1c15372a08d8')
                 .then(response =>  {
                     return response.json()
                 })
                 .then(data => {
-                    return data[0].word
+                    return data.word
                 })
     }
 
