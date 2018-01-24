4aa2b7e30cd54a446ae61560ba9c81a668d60d14
Bradley Landis <bradleylandis@bradleylandis.com>
2018-01-24 13:38:04 -0500

    fix issue where it didn't work if redux devtools weren't installed

--- src/store.js
+++ src/store.js
@@ -1,10 +1,10 @@
 import rootReducer from "./reducers";
-import { createStore, applyMiddleware, compose } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk'
 
 const configureStore = () => {
-    const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    return createStore(rootReducer, compose(applyMiddleware(thunk), reduxDevTools))
+    return createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 }
 
 export default configureStore();
