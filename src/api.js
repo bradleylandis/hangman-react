ee2767753e54f3cb5e208953885b361e06b32b1b
Bradley Landis <blandis@emoneyadvisor.com>
2019-09-26 22:06:11 -0400

    fixing issue with api

--- src/api.js
+++ src/api.js
@@ -1,28 +1,31 @@
 const api = () => {
-    function fetchWord(settings) {
-        const {
-            selectedPartsOfSpeech,
-            minCorpusCount,
-            maxCorpusCount,
-            minDictionaryCount,
-            maxDictionaryCount,
-            minLength,
-            maxLength,
+  function fetchWord(settings) {
+    const {
+      selectedPartsOfSpeech,
+      minCorpusCount,
+      maxCorpusCount,
+      minDictionaryCount,
+      maxDictionaryCount,
+      minLength,
+      maxLength
+    } = settings;
 
-        } = settings
+    return fetch(
+      `https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&includePartOfSpeech=${selectedPartsOfSpeech.join(
+        ","
+      )}&maxCorpusCount=${maxCorpusCount}&minDictionaryCount=${minDictionaryCount}&maxDictionaryCount=${maxDictionaryCount}&minLength=${minLength}&maxLength=${maxLength}&api_key=503890e5c73712c79d3090fb3840a8220541b1c15372a08d8`
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        return data.word;
+      });
+  }
 
-        return fetch(`https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&includePartOfSpeech=${selectedPartsOfSpeech.join(',')}&minCorpusCount=${minCorpusCount}&maxCorpusCount=${maxCorpusCount}&minDictionaryCount=${minDictionaryCount}&maxDictionaryCount=${maxDictionaryCount}&minLength=${minLength}&maxLength=${maxLength}&api_key=503890e5c73712c79d3090fb3840a8220541b1c15372a08d8`)
-                .then(response =>  {
-                    return response.json()
-                })
-                .then(data => {
-                    return data.word
-                })
-    }
+  return {
+    fetchWord: fetchWord
+  };
+};
 
-    return {
-        fetchWord: fetchWord
-    }
-}
-
-export default api()
+export default api();
