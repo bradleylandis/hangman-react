972671394ae509d991b68929483eb09f6066a57b
Bradley T. Landis <bradleylandis@bradleylandis.com>
2019-10-17 19:28:18 -0400

    fix error caused by link without a valid href

--- src/Error.js
+++ src/Error.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Error = ({tryAgain}) => {
-    return <h1>Unable to load a word.  Please <a href="#" onClick={tryAgain}>try again</a></h1>
+    return <h1>Unable to load a word.  Please <button onClick={tryAgain}>try again</button></h1>
 }
 
 export default Error
