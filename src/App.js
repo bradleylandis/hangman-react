1987a63582851f80621dfa7f3d57a395d74476aa
Bradley T. Landis <bradleylandis@bradleylandis.com>
2021-12-31 14:56:16 -0500

    convert App to functional component with hooks

--- src/App.js
+++ src/App.js
@@ -1,54 +1,40 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import * as React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Game from "./Game";
 import "./App.css";
 import { getAppData } from "./appReducer";
 import Loading from "./Loading";
 import Error from "./Error";
 import * as actions from "./actions";
-import PropTypes from "prop-types";
 
-class App extends Component {
-  componentDidMount() {
-    this.props.startGame();
-  }
+const App = () => {
+  const dispatch = useDispatch();
+  const { isLoading, isError } = useSelector(getAppData);
 
-  render() {
-    return (
-      <div className="App">
-        {this.props.isLoading ? (
-          <Loading />
-        ) : this.props.isError ? (
-          <Error tryAgain={() => this.props.startGame()} />
-        ) : (
-          <Game />
-        )}
-        <div>
-          <a
-            href="http://www.wordnik.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img alt="powered by wordnik" src="wordnik.png" />
-          </a>
-        </div>
+  React.useEffect(() => {
+    dispatch(actions.startGame());
+  }, []);
+
+  return (
+    <div className="App">
+      {isLoading ? (
+        <Loading />
+      ) : isError ? (
+        <Error tryAgain={() => dispatch(actions.startGame())} />
+      ) : (
+        <Game />
+      )}
+      <div>
+        <a
+          href="http://www.wordnik.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img alt="powered by wordnik" src="wordnik.png" />
+        </a>
       </div>
-    );
-  }
-}
-
-App.propTypes = {
-  startGame: PropTypes.func.isRequired,
-  isLoading: PropTypes.bool.isRequired,
-  isError: PropTypes.bool.isRequired,
+    </div>
+  );
 };
 
-const mapStateToProps = (state) => {
-  return getAppData(state);
-};
-
-const mapDispatchToProps = {
-  startGame: actions.startGame,
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
