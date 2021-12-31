ba8fd154b238ae06079467e0323758155e485f26
Bradley T. Landis <bradleylandis@bradleylandis.com>
2021-12-31 16:36:38 -0500

    extract variable for api key

--- src/api.js
+++ src/api.js
@@ -1,3 +1,5 @@
+const apiKey = "503890e5c73712c79d3090fb3840a8220541b1c15372a08d8";
+
 export const fetchWord = async (settings) => {
   const {
     selectedPartsOfSpeech,
@@ -11,7 +13,7 @@ export const fetchWord = async (settings) => {
   const response = await fetch(
     `https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&includePartOfSpeech=${selectedPartsOfSpeech.join(
       ","
-    )}&minCorpusCount=10000&maxCorpusCount=${maxCorpusCount}&minDictionaryCount=${minDictionaryCount}&maxDictionaryCount=${maxDictionaryCount}&minLength=${minLength}&maxLength=${maxLength}&api_key=503890e5c73712c79d3090fb3840a8220541b1c15372a08d8`
+    )}&minCorpusCount=10000&maxCorpusCount=${maxCorpusCount}&minDictionaryCount=${minDictionaryCount}&maxDictionaryCount=${maxDictionaryCount}&minLength=${minLength}&maxLength=${maxLength}&api_key=${apiKey}`
   );
   const data = await response.json();
   return data.word;
